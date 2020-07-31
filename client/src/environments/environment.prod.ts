86e24499d3273567ae56b1acb768e1e5731edc13
Yury Vakolov <yury.vakolov@hiq.ru>
2020-08-01 00:18:39 +0300

    added apiURL prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000',
+  apiUrl: 'https://intense-crag-18440.herokuapp.com:5000',
   redirectUri: 'https://intense-crag-18440.herokuapp.com/',
 };
