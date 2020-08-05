3516b3704a7e509eaf33f22f2031558ca58d74da
Yury Vakolov <yury.vakolov@hiq.ru>
2020-08-05 23:10:18 +0300

    add sidebar & top menu

---
+++ client/src/app/pages/directory-page/directory-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directory-page',
+  templateUrl: './directory-page.component.html',
+  styleUrls: ['./directory-page.component.scss']
+})
+export class DirectoryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/pages/live-tape-page/live-tape-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-live-tape-page',
+  templateUrl: './live-tape-page.component.html',
+  styleUrls: ['./live-tape-page.component.scss']
+})
+export class LiveTapePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
